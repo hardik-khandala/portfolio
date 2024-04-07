647ccfac58218ddc32cb72b15710f29b2a0ee8b9
Hardik <hardik.khandala22@gmail.com>
2024-04-07 18:33:31 +0530

    update

--- personal_web/views.py
+++ personal_web/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 
-projects = [
-        {
-            'title': 'Attedence System',
-            'description': "It's used to take attedence of Students using face Recognizations.",
-            'image': '{% static "images/attendece_system.jpg" %}',
-            'github_link': 'https://github.com/hardik-khandala/Attendance-System',
-            'technologies': ['DJANGO', 'PYTHON', 'POSTGRES']
-        },
-        {
-            'title': 'Hand Gesture Detection',
-            'description': "It's used to detect hand gesture movement.",
-            'image': '{% static "images/hand_recog.jpg" %}',
-            'github_link': 'https://github.com/hardik-khandala/Hand-Gesture-detection',
-            'technologies': ['PYTHON', 'OPENCV', 'TENSORFLOW']
-        },
-        {
-            'title': 'Portfolio Django',
-            'description': "Simple my portfolio website for my skills and experience.",
-            'image': '{% static "images/portfolio.png" %}',
-            'github_link': 'https://github.com/hardik-khandala/portfolio',
-            'technologies': ['DJANGO', 'DOCKER', 'AWS']
-        }
-    ]
+# projects = [
+#         {
+#             'title': 'Attedence System',
+#             'description': "It's used to take attedence of Students using face Recognizations.",
+#             'image': '{% static "images/attendece_system.jpg" %}',
+#             'github_link': 'https://github.com/hardik-khandala/Attendance-System',
+#             'technologies': ['DJANGO', 'PYTHON', 'POSTGRES']
+#         },
+#         {
+#             'title': 'Hand Gesture Detection',
+#             'description': "It's used to detect hand gesture movement.",
+#             'image': '{% static "images/hand_recog.jpg" %}',
+#             'github_link': 'https://github.com/hardik-khandala/Hand-Gesture-detection',
+#             'technologies': ['PYTHON', 'OPENCV', 'TENSORFLOW']
+#         },
+#         {
+#             'title': 'Portfolio Django',
+#             'description': "Simple my portfolio website for my skills and experience.",
+#             'image': '{% static "images/portfolio.png" %}',
+#             'github_link': 'https://github.com/hardik-khandala/portfolio',
+#             'technologies': ['DJANGO', 'DOCKER', 'AWS']
+#         }
+#     ]
 
 
 # Create your views here.
@@ -33,7 +33,7 @@ def about(request):
     return render(request, 'about.html', {'title': 'About'})
 
 def workspace(request):
-    context = {
-        'projects': projects
-    }
-    return render(request, 'workspace.html', {'title': 'Workspace'}, context)
+    # context = {
+    #     'projects': projects
+    # }
+    return render(request, 'workspace.html', {'title': 'Workspace'})
